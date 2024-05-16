4a5e6097be01d3f35d8226d3d11980df84e3d4dc
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-15 22:57:58 -0300

    v1/config: cria arquivo que inicia conexao mongodb, rabbitmq e configura graceful shutdown

---
+++ v1/catalog-emit-consumer/src/index.ts
@@ -0,0 +1,37 @@
+import { MongoInstance } from './MongoInstance';
+import { RabbitMQInstance } from './RabbitMQInstance';
+
+RabbitMQInstance.connect().then(() => {});
+MongoInstance.connect().then(() => {});
+
+const onShutdown =
+  (input: { exitCode: number; signalCode: string }) =>
+  async (arg1: any, arg2: any) => {
+    console.log(`Finalizando o processo com o código ${input.signalCode}`);
+
+    if (input.signalCode === 'uncaughtException') {
+      console.log(arg1);
+    }
+
+    if (input.signalCode === 'unhandledRejection') {
+      console.log(arg1);
+      console.log(arg2);
+    }
+
+    await RabbitMQInstance.close();
+    await MongoInstance.close();
+
+    process.exit(input.exitCode);
+  };
+
+process.on('SIGINT', onShutdown({ exitCode: 0, signalCode: 'SIGINT' }));
+process.on('SIGTERM', onShutdown({ exitCode: 0, signalCode: 'SIGTERM' }));
+process.on(
+  'uncaughtException',
+  onShutdown({ exitCode: 1, signalCode: 'uncaughtException' })
+);
+//
+process.on(
+  'unhandledRejection',
+  onShutdown({ exitCode: 1, signalCode: 'unhandledRejection' })
+);
