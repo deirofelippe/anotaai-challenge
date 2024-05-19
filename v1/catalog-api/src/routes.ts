4fcfbb3a4943abe7fed6f24b216a2c8caba692ee
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-19 02:15:19 -0300

    v1/feat(routes): cria rota pra criar product

--- v1/catalog-api/src/routes.ts
+++ v1/catalog-api/src/routes.ts
@@ -3,6 +3,10 @@ import { CreateCategoryController } from './Controllers/CreateCategoryController
 import { CreateCategoryUsecase } from './Usecases/CreateCategoryUsecase';
 import { CategoryRepository } from './Repositories/CategoryRepository';
 import { NewRecordedDataQueue } from './Queues/NewRecordedDataQueue';
+import { OwnerRepository } from './Repositories/OwnerRepository';
+import { CreateProductController } from './Controllers/CreateProductController';
+import { CreateProductUsecase } from './Usecases/CreateProductUsecase';
+import { ProductRepository } from './Repositories/ProductRepository';
 
 const router = express.Router();
 
@@ -28,6 +32,19 @@ const createCategoryController = new CreateCategoryController(
 router.post(
   '/v1/categories',
   createCategoryController.execute.bind(createCategoryController)
+);
+
+const createProductController = new CreateProductController(
+  new CreateProductUsecase({
+    ownerRepository: new OwnerRepository(),
+    categoryRepository: new CategoryRepository(),
+    productRepository: new ProductRepository(),
+    newRecordedDataQueue: new NewRecordedDataQueue()
+  })
+);
+router.post(
+  '/v1/products',
+  createProductController.execute.bind(createProductController)
 );
 
 router.get('/v1/categories', async (req: Request, res: Response) => {
