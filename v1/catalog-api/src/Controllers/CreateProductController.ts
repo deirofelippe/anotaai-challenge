c6f6efefa2a61bf7c33762bdd6b60af34931338e
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-19 02:08:26 -0300

    v1/feat(controller): cria classe para CreateProduct

---
+++ v1/catalog-api/src/Controllers/CreateProductController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import {
+  CreateProductInput,
+  CreateProductOutput,
+  CreateProductUsecase
+} from '../Usecases/CreateProductUsecase';
+
+export class CreateProductController {
+  constructor(private createProductUsecase: CreateProductUsecase) {}
+
+  public async execute(req: Request, res: Response) {
+    const body = req.body as CreateProductInput;
+
+    let result: CreateProductOutput;
+    try {
+      result = await this.createProductUsecase.execute({
+        owner: body.owner,
+        category: body.category,
+        title: body.title,
+        description: body.description,
+        price: body.price
+      });
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .json({ errors: [{ message: 'Internal server error' }] });
+    }
+
+    if (result.errors.length > 0) {
+      return res.status(422).json({ errors: result.errors });
+    }
+
+    return res.status(201).json({});
+  }
+}
