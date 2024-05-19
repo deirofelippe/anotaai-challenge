22f9a0026406d12c77f6c1d1f60e4909fbd934c6
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-19 02:09:48 -0300

    v1/feat(validator): muda mensagens e adiciona type error

--- v1/catalog-api/src/Validators/Schema/CreateCategorySchema.ts
+++ v1/catalog-api/src/Validators/Schema/CreateCategorySchema.ts
@@ -2,17 +2,20 @@ import { object, string } from 'yup';
 
 export const createCategorySchema = object({
   owner: string()
-    .required('Title deve ser preenchido')
+    .required('Owner deve ser preenchido')
+    .typeError('Owner deve conter caracteres')
     .transform((value: string) => value.trim())
-    .min(1, 'Title deve ter no mínimo ${min} caracter')
-    .max(26, 'Title deve ter no máximo ${max} caracteres'),
+    .min(1, 'Owner deve ter no mínimo ${min} caracter')
+    .max(26, 'Owner deve ter no máximo ${max} caracteres'),
   title: string()
     .required('Title deve ser preenchido')
+    .typeError('Title deve conter caracteres')
     .transform((value: string) => value.trim())
     .min(1, 'Title deve ter no mínimo ${min} caracter')
     .max(128, 'Title deve ter no máximo ${max} caracteres'),
   description: string()
-    .nullable()
+    .notRequired()
+    .typeError('Description deve conter caracteres')
     .transform((value: string) => value.trim())
     .max(512, 'Description deve ter no máximo ${max} caracteres')
     .default('')
