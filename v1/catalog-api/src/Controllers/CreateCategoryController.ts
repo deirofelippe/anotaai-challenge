30fd1d9d9aabb401d1fe918222b71b9d289b2232
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-19 02:20:58 -0300

    v1/feat(controller): trata erro no CreateCategory

--- v1/catalog-api/src/Controllers/CreateCategoryController.ts
+++ v1/catalog-api/src/Controllers/CreateCategoryController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import {
   CreateCategoryInput,
+  CreateCategoryOutput,
   CreateCategoryUsecase
 } from '../Usecases/CreateCategoryUsecase';
 
@@ -10,11 +11,20 @@ export class CreateCategoryController {
   public async execute(req: Request, res: Response) {
     const body = req.body as CreateCategoryInput;
 
-    const result = await this.createCategoryUsecase.execute({
-      owner: body.owner,
-      title: body.title,
-      description: body.description
-    });
+    let result: CreateCategoryOutput;
+    try {
+      result = await this.createCategoryUsecase.execute({
+        owner: body.owner,
+        title: body.title,
+        description: body.description
+      });
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .json({ errors: [{ message: 'Internal server error' }] });
+    }
 
     if (result.errors.length > 0) {
       return res.status(422).json({ errors: result.errors });
