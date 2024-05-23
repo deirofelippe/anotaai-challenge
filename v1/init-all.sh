6795469d69ebf5c69afa85130d81422f3b578670
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-22 22:10:33 -0300

    v1/script: inicia docker, terraform e k6

--- v1/init-all.sh
+++ v1/init-all.sh
@@ -6,4 +6,25 @@ ROOT_PATH=$(pwd)
 # docker compose -f ./docker-compose.yaml up -d
 
 cd "$ROOT_PATH"
+
+echo "Iniciando containers..."
 docker compose -f ./docker-compose.yaml up -d
+
+echo "Executando producer..."
+docker compose exec producer npm run dev &
+
+echo "Executando consumer..."
+docker compose exec consumer npm run dev &
+
+cd terraform
+
+echo "Iniciando terraform..."
+terraform init
+
+echo "Construindo infra..."
+terraform apply -auto-approve
+
+cd "$ROOT_PATH"/scripts-k6
+
+echo "Executando teste de carga..."
+./k6 run script.js
