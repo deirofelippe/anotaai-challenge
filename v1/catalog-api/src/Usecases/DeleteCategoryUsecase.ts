51dd77aa986cb4fb547b0531ad3647b25d34f6f2
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-05-26 19:22:37 -0300

    v1/feat(usecase): cria DeleteCategoryUsecase

---
+++ v1/catalog-api/src/Usecases/DeleteCategoryUsecase.ts
@@ -0,0 +1,67 @@
+import { NewRecordedDataQueue } from '../Queues/NewRecordedDataQueue';
+import { CategoryRepository } from '../Repositories/CategoryRepository';
+import { ErrorMessages } from '../types';
+import { OwnerRepository } from '../Repositories/OwnerRepository';
+import { DeleteCategoryValidator } from '../Validators/DeleteCategoryValidator';
+
+export type DeleteCategoryUsecaseInput = {
+  category: string;
+  owner: string;
+};
+
+export type DeleteCategoryUsecaseOutput = {
+  errors: ErrorMessages['errors'];
+};
+
+export type DeleteCategoryUsecaseConstructor = {
+  ownerRepository: OwnerRepository;
+  categoryRepository: CategoryRepository;
+  newRecordedDataQueue: NewRecordedDataQueue;
+};
+
+export class DeleteCategoryUsecase {
+  constructor(
+    private createCategoryUsecaseConstructor: DeleteCategoryUsecaseConstructor
+  ) {}
+
+  public async execute(
+    input: DeleteCategoryUsecaseInput
+  ): Promise<DeleteCategoryUsecaseOutput> {
+    const { categoryRepository, ownerRepository, newRecordedDataQueue } =
+      this.createCategoryUsecaseConstructor;
+
+    const validator = new DeleteCategoryValidator();
+    const { newData, errors } = validator.validate(input);
+
+    if (errors.length > 0) {
+      return { errors: errors };
+    }
+
+    const category: DeleteCategoryUsecaseInput = {
+      owner: newData!.owner,
+      category: newData!.category
+    };
+
+    const ownerFound = await ownerRepository.findOwner({
+      owner: category.owner
+    });
+
+    if (ownerFound.length <= 0) {
+      return { errors: [{ message: 'Owner não existe' }] };
+    }
+
+    const categoryFound = await categoryRepository.findCategoryByTitle({
+      owner: category.owner,
+      title: category.category
+    });
+
+    if (categoryFound.length <= 0) {
+      return { errors: [{ message: 'Categoria não existe' }] };
+    }
+
+    await categoryRepository.deleteCategory(category);
+    await newRecordedDataQueue.sendMessage({ owner: category.owner });
+
+    return { errors: [] };
+  }
+}
