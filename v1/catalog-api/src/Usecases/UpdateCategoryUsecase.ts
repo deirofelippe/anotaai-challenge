90111e9e74a2503f2bd7f6fab5ae99676d3b680e
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-06-02 22:34:38 -0300

    v1/feat(usecase): cria UpdateCategoryUsecase

---
+++ v1/catalog-api/src/Usecases/UpdateCategoryUsecase.ts
@@ -0,0 +1,76 @@
+import { NewRecordedDataQueue } from '../Queues/NewRecordedDataQueue';
+import {
+  CategoryRepository,
+  UpdateCategoryRepositoryInput
+} from '../Repositories/CategoryRepository';
+import { ErrorMessages } from '../types';
+import { OwnerRepository } from '../Repositories/OwnerRepository';
+import { UpdateCategoryValidator } from '../Validators/UpdateCategoryValidator';
+import { log } from '../Config/Logger';
+
+export type UpdateCategoryUsecaseInput = {
+  owner: string;
+  category: string;
+  fields: {
+    title?: string;
+    description?: string;
+  };
+};
+
+export type UpdateCategoryUsecaseOutput = {
+  errors: ErrorMessages['errors'];
+};
+
+export type UpdateCategoryUsecaseConstructor = {
+  ownerRepository: OwnerRepository;
+  categoryRepository: CategoryRepository;
+  newRecordedDataQueue: NewRecordedDataQueue;
+};
+
+export class UpdateCategoryUsecase {
+  constructor(
+    private createCategoryUsecaseConstructor: UpdateCategoryUsecaseConstructor
+  ) {}
+
+  public async execute(
+    input: UpdateCategoryUsecaseInput
+  ): Promise<UpdateCategoryUsecaseOutput> {
+    const { categoryRepository, ownerRepository, newRecordedDataQueue } =
+      this.createCategoryUsecaseConstructor;
+
+    const validator = new UpdateCategoryValidator();
+    const { newData, errors } = validator.validate(input);
+
+    if (errors.length > 0) {
+      return { errors: errors };
+    }
+
+    const newCategory: UpdateCategoryRepositoryInput = {
+      owner: newData!.owner,
+      category: newData!.category,
+      fields: newData!.fields
+    };
+
+    const ownerFound = await ownerRepository.findOwner({
+      owner: newCategory.owner
+    });
+
+    if (ownerFound.length <= 0) {
+      return { errors: [{ message: 'Owner não existe' }] };
+    }
+
+    const categoryFound = await categoryRepository.findCategoryByTitle({
+      owner: newCategory.owner,
+      title: newCategory.category
+    });
+
+    if (categoryFound.length <= 0) {
+      return { errors: [{ message: 'Categoria não existe' }] };
+    }
+
+    await categoryRepository.updateCategory(newCategory);
+    await newRecordedDataQueue.sendMessage({ owner: newCategory.owner });
+
+    return { errors: [] };
+  }
+}
