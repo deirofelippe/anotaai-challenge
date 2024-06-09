d217ab210148e677e1b89d710dce61fb4a78ea85
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-06-09 15:59:04 -0300

    v1/test(integration): cria teste para UpdateProductUsecase

--- v1/catalog-api/__tests__/integration/update-product.spec.ts
+++ v1/catalog-api/__tests__/integration/update-product.spec.ts
@@ -8,6 +8,8 @@ import {
   UpdateProductUsecase,
   UpdateProductUsecaseInput
 } from '../../src/Usecases/UpdateProductUsecase';
+import { CategoryRepository } from '../../src/Repositories/CategoryRepository';
+import { log } from '../../src/Config/Logger';
 
 describe('UpdateProductUsecase', () => {
   let mongoInstance: Collection<Document>;
@@ -25,10 +27,11 @@ describe('UpdateProductUsecase', () => {
     await RabbitMQSingleton.close();
   });
 
-  describe('Update category', () => {
+  describe('Update product', () => {
     const updateProductUsecase = new UpdateProductUsecase({
       ownerRepository: new OwnerRepository(),
-      categoryRepository: new ProductRepository(),
+      productRepository: new ProductRepository(),
+      categoryRepository: new CategoryRepository(),
       newRecordedDataQueue: new NewRecordedDataQueue()
     });
 
@@ -46,11 +49,6 @@ describe('UpdateProductUsecase', () => {
                   title: 'RTX 3060',
                   description: 'Placa de vídeo',
                   price: 1699.99
-                },
-                {
-                  title: 'Ryzen 5 5600X',
-                  description: 'Processador',
-                  price: 1039.99
                 }
               ]
             }
@@ -64,128 +62,165 @@ describe('UpdateProductUsecase', () => {
       jest.restoreAllMocks();
     });
 
-    let category: UpdateProductUsecaseInput;
+    let product: UpdateProductUsecaseInput;
 
     beforeEach(() => {
-      category = {
+      product = {
         owner: '321',
+        product: 'RTX 3060',
         category: 'Computador',
         fields: {
           title: 'Title atualizado',
-          description: 'Description atualizado'
+          description: 'Description atualizado',
+          price: 1700.5
         }
       };
     });
 
-    test('Deve atualizar title e description de categoria', async () => {
+    test('Deve atualizar title, description e price de produto', async () => {
       const sendMessageMock = jest
         .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
         .mockImplementation(async () => {});
 
       const createdCatalogBefore = await mongoInstance.find().toArray();
-      const output = await updateProductUsecase.execute(category);
+      const output = await updateProductUsecase.execute(product);
       const createdCatalogAfter = await mongoInstance.find().toArray();
 
       const inputQueue = sendMessageMock.mock.calls[0][0];
 
-      const categoryBefore = createdCatalogBefore[0].catalog[0];
-      const categoryAfter = createdCatalogAfter[0].catalog[0];
+      const productBefore = createdCatalogBefore[0].catalog[0].itens[0];
+      const productAfter = createdCatalogAfter[0].catalog[0].itens[0];
 
-      expect(categoryBefore).not.toEqual(
+      expect(productBefore).not.toEqual(
         expect.objectContaining({
-          category_description: category.fields.description,
-          category_title: category.fields.title
+          description: product.fields.description,
+          title: product.fields.title,
+          price: product.fields.price
         })
       );
-      expect(categoryAfter).toEqual(
+      expect(productAfter).toEqual(
         expect.objectContaining({
-          category_description: category.fields.description,
-          category_title: category.fields.title
+          description: product.fields.description,
+          title: product.fields.title,
+          price: product.fields.price
         })
       );
-      expect(inputQueue.owner).toEqual(category.owner);
+      expect(inputQueue.owner).toEqual(product.owner);
       expect(output.errors).toHaveLength(0);
     });
 
-    test('Deve atualizar title de categoria', async () => {
-      delete category.fields.description;
-
-      const sendMessageMock = jest
-        .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
-        .mockImplementation(async () => {});
-
-      const createdCatalogBefore = await mongoInstance.find().toArray();
-      const output = await updateProductUsecase.execute(category);
-      const createdCatalogAfter = await mongoInstance.find().toArray();
-
-      const inputQueue = sendMessageMock.mock.calls[0][0];
-
-      const categoryBefore = createdCatalogBefore[0].catalog[0];
-      const categoryAfter = createdCatalogAfter[0].catalog[0];
-
-      expect(categoryBefore).not.toEqual(
-        expect.objectContaining({
-          category_title: category.fields.title
-        })
-      );
-      expect(categoryAfter).toEqual(
-        expect.objectContaining({
-          category_title: category.fields.title
-        })
-      );
-      expect(categoryBefore).toEqual(
-        expect.objectContaining({
-          category_description: categoryAfter.category_description
-        })
-      );
-      expect(inputQueue.owner).toEqual(category.owner);
-      expect(output.errors).toHaveLength(0);
-    });
-
-    test('Deve atualizar description de categoria', async () => {
-      delete category.fields.title;
+    test('Deve atualizar title de produto', async () => {
+      delete product.fields.description;
+      delete product.fields.price;
 
       const sendMessageMock = jest
         .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
         .mockImplementation(async () => {});
 
       const createdCatalogBefore = await mongoInstance.find().toArray();
-      const output = await updateProductUsecase.execute(category);
+      const output = await updateProductUsecase.execute(product);
       const createdCatalogAfter = await mongoInstance.find().toArray();
 
       const inputQueue = sendMessageMock.mock.calls[0][0];
 
-      const categoryBefore = createdCatalogBefore[0].catalog[0];
-      const categoryAfter = createdCatalogAfter[0].catalog[0];
+      const productBefore = createdCatalogBefore[0].catalog[0].itens[0];
+      const productAfter = createdCatalogAfter[0].catalog[0].itens[0];
 
-      expect(categoryBefore).not.toEqual(
+      expect(productAfter).toEqual(
         expect.objectContaining({
-          category_description: category.fields.description
+          title: product.fields.title
         })
       );
-      expect(categoryAfter).toEqual(
+
+      expect(productBefore).toEqual(
         expect.objectContaining({
-          category_description: category.fields.description
+          description: productAfter.description,
+          price: productAfter.price
         })
       );
-      expect(categoryBefore).toEqual(
-        expect.objectContaining({
-          category_title: categoryAfter.category_title
-        })
-      );
-      expect(inputQueue.owner).toEqual(category.owner);
+
+      expect(inputQueue.owner).toEqual(product.owner);
       expect(output.errors).toHaveLength(0);
     });
 
-    test('Não deve atualizar categoria sem campos para atualizar', async () => {
-      delete category.fields.title;
-      delete category.fields.description;
+    test('Deve atualizar description de produto', async () => {
+      delete product.fields.title;
+      delete product.fields.price;
 
       const sendMessageMock = jest
         .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
         .mockImplementation(async () => {});
 
-      const output = await updateProductUsecase.execute(category);
+      const createdCatalogBefore = await mongoInstance.find().toArray();
+      const output = await updateProductUsecase.execute(product);
+      const createdCatalogAfter = await mongoInstance.find().toArray();
+
+      const inputQueue = sendMessageMock.mock.calls[0][0];
+
+      const productBefore = createdCatalogBefore[0].catalog[0].itens[0];
+      const productAfter = createdCatalogAfter[0].catalog[0].itens[0];
+
+      expect(productAfter).toEqual(
+        expect.objectContaining({
+          description: product.fields.description
+        })
+      );
+
+      expect(productBefore).toEqual(
+        expect.objectContaining({
+          title: productAfter.title,
+          price: productAfter.price
+        })
+      );
+
+      expect(inputQueue.owner).toEqual(product.owner);
+      expect(output.errors).toHaveLength(0);
+    });
+
+    test('Deve atualizar price de produto', async () => {
+      delete product.fields.title;
+      delete product.fields.description;
+
+      const sendMessageMock = jest
+        .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
+        .mockImplementation(async () => {});
+
+      const createdCatalogBefore = await mongoInstance.find().toArray();
+      const output = await updateProductUsecase.execute(product);
+      const createdCatalogAfter = await mongoInstance.find().toArray();
+
+      const inputQueue = sendMessageMock.mock.calls[0][0];
+
+      const productBefore = createdCatalogBefore[0].catalog[0].itens[0];
+      const productAfter = createdCatalogAfter[0].catalog[0].itens[0];
+
+      expect(productAfter).toEqual(
+        expect.objectContaining({
+          price: product.fields.price
+        })
+      );
+
+      expect(productBefore).toEqual(
+        expect.objectContaining({
+          title: productAfter.title,
+          description: productAfter.description
+        })
+      );
+
+      expect(inputQueue.owner).toEqual(product.owner);
+      expect(output.errors).toHaveLength(0);
+    });
+
+    test('Não deve atualizar produto sem campos para atualizar', async () => {
+      delete product.fields.title;
+      delete product.fields.description;
+      delete product.fields.price;
+
+      const sendMessageMock = jest
+        .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
+        .mockImplementation(async () => {});
+
+      const output = await updateProductUsecase.execute(product);
 
       expect(output.errors[0].message).toEqual(
         'Algum campo deve ser preenchido'
@@ -194,26 +229,39 @@ describe('UpdateProductUsecase', () => {
     });
 
     test('Não deve atualizar categoria não existe', async () => {
-      category.category = '111';
+      product.category = '111';
 
       const sendMessageMock = jest
         .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
         .mockImplementation(async () => {});
 
-      const output = await updateProductUsecase.execute(category);
+      const output = await updateProductUsecase.execute(product);
 
       expect(output.errors[0].message).toEqual('Categoria não existe');
       expect(sendMessageMock).toHaveBeenCalledTimes(0);
     });
 
-    test('Não deve atualizar owner não existe', async () => {
-      category.owner = 'Teste';
+    test('Não deve atualizar produto não existe', async () => {
+      product.product = '111';
 
       const sendMessageMock = jest
         .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
         .mockImplementation(async () => {});
 
-      const output = await updateProductUsecase.execute(category);
+      const output = await updateProductUsecase.execute(product);
+
+      expect(output.errors[0].message).toEqual('Produto não existe');
+      expect(sendMessageMock).toHaveBeenCalledTimes(0);
+    });
+
+    test('Não deve atualizar owner não existe', async () => {
+      product.owner = 'Teste';
+
+      const sendMessageMock = jest
+        .spyOn(NewRecordedDataQueue.prototype, 'sendMessage')
+        .mockImplementation(async () => {});
+
+      const output = await updateProductUsecase.execute(product);
 
       expect(output.errors[0].message).toEqual('Owner não existe');
       expect(sendMessageMock).toHaveBeenCalledTimes(0);
