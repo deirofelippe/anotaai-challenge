5e6effee53ecab0e87a28b03fd34cd1a88023ee6
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-07-14 20:33:17 -0300

    v1/feat(controller): cria controller para update category

---
+++ v1/catalog-api/src/Controllers/UpdateCategoryController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import {
+  UpdateCategoryUsecaseInput,
+  UpdateCategoryUsecaseOutput,
+  UpdateCategoryUsecase
+} from '../Usecases/UpdateCategoryUsecase';
+
+export class UpdateCategoryController {
+  constructor(private createCategoryUsecase: UpdateCategoryUsecase) {}
+
+  public async execute(req: Request, res: Response) {
+    const body = req.body as UpdateCategoryUsecaseInput;
+
+    let result: UpdateCategoryUsecaseOutput;
+    try {
+      result = await this.createCategoryUsecase.execute({
+        owner: body.owner,
+        category: body.category,
+        fields: body.fields
+      });
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .json({ errors: [{ message: 'Internal server error' }] });
+    }
+
+    if (result.errors.length > 0) {
+      return res.status(422).json({ errors: result.errors });
+    }
+    return res.status(200).json({});
+  }
+}
