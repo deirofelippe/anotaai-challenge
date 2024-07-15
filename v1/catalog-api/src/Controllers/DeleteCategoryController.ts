94528cc60d17fb9b0121888ed233ff2bf1325223
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-07-14 23:42:31 -0300

    v1/feat(controller): cria controller para delete category

---
+++ v1/catalog-api/src/Controllers/DeleteCategoryController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import {
+  DeleteCategoryUsecaseInput,
+  DeleteCategoryUsecaseOutput,
+  DeleteCategoryUsecase
+} from '../Usecases/DeleteCategoryUsecase';
+
+export class DeleteCategoryController {
+  constructor(private deleteCategoryUsecase: DeleteCategoryUsecase) {}
+
+  public async execute(req: Request, res: Response) {
+    let result: DeleteCategoryUsecaseOutput;
+
+    try {
+      result = await this.deleteCategoryUsecase.execute({
+        owner: req.query.owner as string,
+        category: req.query.title as string
+      });
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .json({ errors: [{ message: 'Internal server error' }] });
+    }
+
+    if (result.errors.length > 0) {
+      return res.status(422).json({ errors: result.errors });
+    }
+    return res.status(204).json({});
+  }
+}
