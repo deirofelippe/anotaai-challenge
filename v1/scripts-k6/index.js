4076ad137e63c0f737fb1c7967aba055dee8eb78
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-07-14 21:30:52 -0300

    v1/feat(k6): cria teste k6 pra create category, create product, update category

---
+++ v1/scripts-k6/index.js
@@ -0,0 +1,157 @@
+import { group } from "k6";
+import exec from "k6/execution";
+import { Counter } from "k6/metrics";
+
+import faker from "k6/x/faker";
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export const options = {
+  stages: [
+    { duration: "2s", target: 25 },
+    { duration: "2s", target: 25 },
+    { duration: "1s", target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ["p(95) < 2000"], // 95% das requisições devem responder em até 2s
+    http_req_failed: ["rate < 0.01"], // 1% das requisições podem falhar
+  },
+};
+
+const metricStatusCode200 = new Counter("status_code_200");
+const metricStatusCode201 = new Counter("status_code_201");
+const metricStatusCode204 = new Counter("status_code_204");
+const metricStatusCode422 = new Counter("status_code_500");
+const metricStatusCode500 = new Counter("status_code_422");
+
+function addMetricsStatusCode(statusCode) {
+  switch (statusCode) {
+    case 200:
+      metricStatusCode200.add(1);
+      break;
+    case 201:
+      metricStatusCode201.add(1);
+      break;
+    case 204:
+      metricStatusCode204.add(1);
+      break;
+    case 422:
+      metricStatusCode422.add(1);
+      break;
+    case 500:
+      metricStatusCode500.add(1);
+      break;
+
+    default:
+      break;
+  }
+}
+
+function generateCategory() {
+  const category = {
+    owner: faker.number.number(1, 15),
+    title: faker.product.productName(),
+    description: faker.product.productDescription(),
+  };
+
+  return category;
+}
+
+function generateProduct(catalog) {
+  const product = {
+    owner: catalog.owner,
+    category: catalog.category,
+    title: faker.product.productName(),
+    description: faker.product.productDescription(),
+    price: faker.number.float32Range(50, 5000).toFixed(2),
+  };
+
+  return product;
+}
+
+export default function () {
+  const category = generateCategory();
+  const product = generateProduct({
+    owner: category.owner,
+    category: category.title,
+  });
+
+  group("1. Create new category", () => {
+    const payload = JSON.stringify(category);
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const res = http.post("http://localhost:3000/v1/categories", payload, {
+      headers,
+    });
+
+    check(res, {
+      "is status code 2xx": (r) => `${r.status}`.startsWith("2"),
+    });
+
+    addMetricsStatusCode(res.status);
+
+    sleep(1);
+  });
+
+  group("2. Create new product", () => {
+    const payload = JSON.stringify(product);
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const res = http.post("http://localhost:3000/v1/products", payload, {
+      headers,
+    });
+
+    check(res, {
+      "is status code 2xx": (r) => `${r.status}`.startsWith("2"),
+    });
+
+    addMetricsStatusCode(res.status);
+
+    sleep(1);
+  });
+
+  group("3. Update category", () => {
+    const payload = JSON.stringify({
+      owner: category.owner,
+      category: category.title,
+      fields: {
+        title: category.title + " Atualizado",
+        description: category.description + " Atualizado",
+      },
+    });
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const res = http.patch("http://localhost:3000/v1/categories", payload, {
+      headers,
+    });
+
+    check(res, {
+      "is status code 2xx": (r) => `${r.status}`.startsWith("2"),
+    });
+
+    addMetricsStatusCode(res.status);
+
+    sleep(1);
+  });
+
+  // group("4. Update product", () => {
+  //   CreateCategory();
+  // });
+
+  // group("5. Delete category", () => {
+  //   CreateCategory();
+  // });
+
+  // group("6. Delete product", () => {
+  //   CreateCategory();
+  // });
+}
--- v1/scripts-k6/requests/category.js
+++ v1/scripts-k6/requests/category.js
@@ -1,28 +1,35 @@
 import faker from "k6/x/faker";
 import http from "k6/http";
-import { sleep } from "k6";
+import { sleep, check } from "k6";
 
 function generateCategory() {
   const category = JSON.stringify({
     owner: faker.number.number(1, 15),
     title: faker.product.productName(),
     description: faker.product.productDescription(),
-    itens: [],
   });
 
   return category;
 }
 
-export function createCategory() {
+export function CreateCategory() {
   const payload = generateCategory();
 
   const headers = {
     "Content-Type": "application/json",
   };
 
-  http.post("http://localhost:3000/v1/categories", payload, { headers });
+  const res = http.post("http://localhost:3000/v1/categories", payload, {
+    headers,
+  });
+
+  check(res, {
+    "is status 200": (r) => r.status === 201,
+  });
+
+  sleep(1);
 }
 
-export function deleteCategory() {}
+export function DeleteCategory() {}
 
-export function updateCategory() {}
+export function UpdateCategory() {}
