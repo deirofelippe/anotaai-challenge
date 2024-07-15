a72de8ddeb723c09c0ef1ff1845f2eebc8550b55
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-07-15 00:09:21 -0300

    v1/feat(routes): cria rota para delete product

--- v1/catalog-api/src/Routes/products.routes.ts
+++ v1/catalog-api/src/Routes/products.routes.ts
@@ -7,6 +7,8 @@ import { ProductRepository } from '../Repositories/ProductRepository';
 import { NewRecordedDataQueue } from '../Queues/NewRecordedDataQueue';
 import { UpdateProductController } from '../Controllers/UpdateProductController';
 import { UpdateProductUsecase } from '../Usecases/UpdateProductUsecase';
+import { DeleteProductController } from '../Controllers/DeleteProductController';
+import { DeleteProductUsecase } from '../Usecases/DeleteProductUsecase';
 
 const router = express.Router();
 
@@ -34,6 +36,19 @@ const updateProductController = new UpdateProductController(
 router.patch(
   '/v1/products/:title',
   updateProductController.execute.bind(updateProductController)
+);
+
+const deleteProductController = new DeleteProductController(
+  new DeleteProductUsecase({
+    ownerRepository: new OwnerRepository(),
+    categoryRepository: new CategoryRepository(),
+    productRepository: new ProductRepository(),
+    newRecordedDataQueue: new NewRecordedDataQueue()
+  })
+);
+router.delete(
+  '/v1/products',
+  deleteProductController.execute.bind(deleteProductController)
 );
 
 const productRoutes = router;
