47f09b7fe874feca5247b0eaf2f9dd6406eb60e5
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-07-28 18:34:32 -0300

    v1/config(script): atualiza script de init e finish

--- v1/finish-all.sh
+++ v1/finish-all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-ROOT_PATH=$(pwd)
+echo "Fechando containers principais..."
+docker compose -f ./docker-compose-main.yaml down
 
-cd "$ROOT_PATH"
-docker compose -f ./docker-compose.yaml down
+echo "Fechando containers de observability..."
+docker compose -f ./docker-compose-observability.yaml down
+
+echo "Removendo network..."
+docker network rm desafio
--- v1/init-all.sh
+++ v1/init-all.sh
@@ -2,26 +2,26 @@
 
 ROOT_PATH=$(pwd)
 
-# cd "$ROOT_PATH"/signoz/clickhouse-setup/
-# docker compose -f ./docker-compose.yaml up -d
-
 cd "$ROOT_PATH"
 
 echo "Criando network..."
-docker network create --driver bridge challenge
+docker network create --driver bridge desafio
 
-echo "Iniciando containers..."
-docker compose -f ./docker-compose.yaml up -d
+echo "Iniciando containers principais..."
+docker compose -f ./docker-compose-main.yaml up -d
+
+echo "Iniciando containers de observability..."
+docker compose -f ./docker-compose-observability.yaml up -d
 
 cd terraform
 
 echo "Iniciando terraform..."
 terraform init
 
-echo "Construindo infra..."
-terraform apply -auto-approve
+# echo "Construindo infra..."
+# terraform apply -auto-approve
 
-cd "$ROOT_PATH"/scripts-k6
+cd "$ROOT_PATH"
 
 echo "Executando teste de carga..."
 make k6-run-bin
