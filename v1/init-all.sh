74bb293741c5d023170a331ccd9b0379d5675433
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-10-05 18:13:16 -0300

    v1/script(init-all): inicia os containers com --build e descomenta terraform

--- v1/init-all.sh
+++ v1/init-all.sh
@@ -8,18 +8,18 @@ echo "Criando network..."
 docker network create --driver bridge desafio
 
 echo "Iniciando containers principais..."
-docker compose -f ./docker-compose-main.yaml up -d
+docker compose -f ./docker-compose-main.yaml up -d --build
 
 echo "Iniciando containers de observability..."
-docker compose -f ./docker-compose-observability.yaml up -d
+docker compose -f ./docker-compose-observability.yaml up -d --build
 
 cd terraform
 
 echo "Iniciando terraform..."
 terraform init
 
-# echo "Construindo infra..."
-# terraform apply -auto-approve
+echo "Construindo infra..."
+terraform apply -auto-approve
 
 cd "$ROOT_PATH"
 
