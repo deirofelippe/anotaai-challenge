3226757492d5090369d098dcd2c429c68a61992f
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-10-23 16:44:04 -0300

    v1/feat(queue): adiciona logs na queue

--- v1/catalog-api/src/Queues/NewRecordedDataQueue.ts
+++ v1/catalog-api/src/Queues/NewRecordedDataQueue.ts
@@ -1,9 +1,18 @@
+import { logger } from '../Config/Logger';
 import { RabbitMQSingleton } from '../Config/RabbitMQSingleton';
 
 export type NewRecordedDataQueueInput = { owner: string };
 
 export class NewRecordedDataQueue {
   public async sendMessage(input: NewRecordedDataQueueInput) {
+    logger.info({
+      context: 'repository',
+      data: 'Iniciando o NewRecordedDataQueue.sendMessage'
+    });
+    logger.info({
+      context: 'repository',
+      data: { input }
+    });
     const publisher = RabbitMQSingleton.getPublisher()!;
 
     try {
@@ -17,10 +26,17 @@ export class NewRecordedDataQueue {
         },
         input
       );
-      return;
+
+      logger.info({
+        context: 'repository',
+        data: 'Finalizando o NewRecordedDataQueue.sendMessage'
+      });
     } catch (error) {
-      console.error('Erro ao envia mensagem para a fila');
-      console.error('Dados: ', JSON.stringify(input));
+      logger.error({
+        context: 'repository',
+        data: 'Erro no NewRecordedDataQueue.sendMessage'
+      });
+      logger.error({ context: 'repository', data: error });
       throw error;
     }
   }
