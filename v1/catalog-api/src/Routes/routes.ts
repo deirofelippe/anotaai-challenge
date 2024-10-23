97ee58a4a9cccf1508baa45356c11391886f4f0e
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-10-23 16:46:47 -0300

    v1/feat(prometheus): configura rota para expor as metricas

--- v1/catalog-api/src/Routes/routes.ts
+++ v1/catalog-api/src/Routes/routes.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from 'express';
 import { CategoryRepository } from '../Repositories/CategoryRepository';
+import { logger } from '../Config/Logger';
+import { register } from '../Config/prometheus';
 
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+router.get('/metrics', async (req: Request, res: Response) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
 });
 
 router.delete('/clear-db', async (req: Request, res: Response) => {
   try {
     await new CategoryRepository().dropCollection();
+
     return res.status(204).json({});
   } catch (error) {
     return res.status(500).json({ message: 'Erro no servidor' });
@@ -19,6 +27,7 @@ router.delete('/clear-db', async (req: Request, res: Response) => {
 router.get('/v1/all', async (req: Request, res: Response) => {
   try {
     const all = await new CategoryRepository().findAll();
+
     return res.status(200).json(all);
   } catch (error) {
     return res.status(500).json({ message: 'Erro no servidor' });
@@ -30,6 +39,7 @@ router.get('/v1/owners/:id/categories', async (req: Request, res: Response) => {
     const categories = await new CategoryRepository().findCategoriesByOwner({
       owner: req.params.id
     });
+
     return res.status(200).json(categories);
   } catch (error) {
     return res.status(500).json({ message: 'Erro no servidor' });
